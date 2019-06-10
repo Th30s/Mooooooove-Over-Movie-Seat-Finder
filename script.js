8206e5cca9d518284d004138a37342c84456ad91
Th30s <43751800+Th30s@users.noreply.github.com>
2019-06-10 10:07:11 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+var movieReview = [
+        "The best movie this year! ",
+        "I laughed, I cried, then I laughed again! ",
+        "I could not sleep after seeing this movie! "
+    ];
+$("img").click(function(){
+    movieReview.forEach(function(reviews) {
+        $("#movieReviews").append(reviews);
+    });
+ 
+});
+$("#calculateTotal").click(function(){
+    var child = $("#numberChild").val();
+    var adult = $("#numberAdult").val();
+    var total = child*8 + adult*12;
+    $("#totalCost").text( "$" + total);
+});
+if (child<0) {
+    $
+}
+    
+    
+    
+
+
